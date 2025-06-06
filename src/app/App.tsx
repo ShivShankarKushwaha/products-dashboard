da362916b51920b9f333f077fd5ee149d3d1beb3
ShivShankarKushwaha <shivshankarkushwaha0000@gmail.com>
2025-06-06 23:53:37 +0530

    add: footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import AppRoutes from "./routes";
+import Footer from "../components/Footer";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar />
-        <main className="p-4">
+        <main className="p-4 min-h-[85.8vh]">
           <AppRoutes />
         </main>
+        <Footer />
       </div>
     </Router>
   );
